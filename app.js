b182b054d71f25d817f00ec5ad2767454a76020b
Sebastian Jara <s.jaranilsson@gmail.com>
2022-07-10 14:13:08 -0400

    app ready

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const categoryList = document.getElementById("category-list");
 const cards = document.getElementById("cards-grid");
 const form = document.getElementById('form');
 
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
 // FUNCTIONS TO BE REUSED
 const getData = (url) => {
   return fetch(url)
@@ -31,7 +33,7 @@ const removeSelectedList = (pathDOM) => {
   };
 };
 
-// PROGRAMATICAL CREATION OF HTML ELEMENTS
+// CREATION OF HTML ELEMENTS
 const categoryListItem = (category) => {
   let li = document.createElement("li");
   li.classList.add("list-group-item");
@@ -50,7 +52,19 @@ const productCard = (product) => {
   return div;
 };
 
-// FILLING LIST AND CARDS
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+// FILLING LIST AND CARDS - initialization
 append(categoriesURL, categoryList, categoryListItem);
 append(productsURL, cards, productCard);
 
@@ -59,14 +73,29 @@ categoryList.addEventListener('click', (e) => {
   removeSelectedList(categoryList.children);
   e.target.classList.add('active');
   cards.innerHTML = '';
-  const url = `https://api-bsale-seb.herokuapp.com/api/v1/categories/${e.target.innerHTML}`;
-  append(url, cards, productCard);
+  if(e.target.innerHTML === 'todos los productos') {
+    const url = productsURL;
+    append(url, cards, productCard);
+  } else {
+    const url = `https://api-bsale-seb.herokuapp.com/api/v1/categories/${e.target.innerHTML}`
+    append(url, cards, productCard);
+  }
+  // const url = `https://api-bsale-seb.herokuapp.com/api/v1/categories/${e.target.innerHTML}`;
+  // append(url, cards, productCard);
 });
 
 // ADDING SEARCH BAR FUNCTIONALITY
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', () => {
   removeSelectedList(categoryList.children);
   cards.innerHTML = '';
-  const url = `https://api-bsale-seb.herokuapp.com/api/v1/products/${document.querySelector('input').value}`;
+  const url = `https://api-bsale-seb.herokuapp.com/api/v1/products/${document.querySelector('input').value}`
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    if (data.length === 0) {
+      alert('No tenemos productos con ese nombre!', 'warning')
+    }
+  });
   append(url, cards, productCard);
-});
+  form.reset();
+})
